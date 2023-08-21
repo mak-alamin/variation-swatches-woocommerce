513dac3c73c6465c0324547b70451055ae78bdc1
Mak Alamin <makalamin012@gmail.com>
2023-08-22 05:22:27 +0600

    Initial commit

---
+++ variation-swatches-woocommerce.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* Plugin Name: WooCommerce Variation Swatches
+* Plugin URI: https://www.makalamin.com
+* Description: Wonderful variation swatches for WooCommerce products.
+* Version: 1.0.0
+* Requires at least: 5.2
+* Requires PHP: 7.2
+* Author: Mak Alamin
+* Author URI:
+* License: GPL v2 or later
+* License URI: https: //www.gnu.org/licenses/gpl-2.0.html
+* Update URI: 
+* Text Domain: variation-swatches-woocommerce
+* Domain Path: /languages
+*/
